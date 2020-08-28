4f95172615e34d7fcf4f1c5a35d77e5a9ffa68b1
Adriano Gonçalves <adriangonc@yahoo.com.br>
2020-08-28 10:12:14 -0300

    Configuring check style with prettier.

--- test/globals.d.ts
+++ test/globals.d.ts
@@ -1,5 +1,6 @@
 declare namespace NodeJS {
-    interface Global { //Add testRequest type to Global interface of nodejs.
-        testRequest: import('supertest').SuperTest<import('supertest').Test> //Needs to be imported inline in order to be recognized as a global configuration instead of local.
-    }
+  interface Global {
+    //Add testRequest type to Global interface of nodejs.
+    testRequest: import('supertest').SuperTest<import('supertest').Test>; //Needs to be imported inline in order to be recognized as a global configuration instead of local.
+  }
 }
--- test/jest-setup.ts
+++ test/jest-setup.ts
@@ -1,9 +1,8 @@
 import { SetupServer } from '@src/server'; //Alias used to access files
-import supertest from 'supertest'
+import supertest from 'supertest';
 
 beforeAll(() => {
-    const server = new SetupServer();
-    server.init();
-    global.testRequest = supertest(server.getApp());
+  const server = new SetupServer();
+  server.init();
+  global.testRequest = supertest(server.getApp());
 });
-
